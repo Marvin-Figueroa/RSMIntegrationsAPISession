2838a89dc91b50d0648fb67343084199aed8824a
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-07 16:56:10 -0600

    feat: add validators for the product dtos

---
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Infrastructure/Validators/CreateProductDtoValidator.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+using RSMEnterpriseIntegrationsAPI.Application.DTOs;
+
+public class CreateProductDtoValidator : AbstractValidator<CreateProductDto>
+{
+  public CreateProductDtoValidator()
+  {
+    RuleFor(dto => dto.Name).NotEmpty().WithMessage("Name is required.");
+    RuleFor(dto => dto.ProductNumber).NotEmpty().WithMessage("Product number is required.");
+    RuleFor(dto => dto.SafetyStockLevel).GreaterThan((short)0).WithMessage("Safety stock level must be greater than 0.");
+    RuleFor(dto => dto.ReorderPoint).GreaterThan((short)0).WithMessage("Reorder point must be greater than 0.");
+    RuleFor(dto => dto.StandardCost).GreaterThanOrEqualTo(0).WithMessage("Standard cost must be greater than or equal to 0.");
+    RuleFor(dto => dto.ListPrice).GreaterThanOrEqualTo(0).WithMessage("List price must be greater than or equal to 0.");
+    RuleFor(dto => dto.DaysToManufacture).GreaterThan(0).WithMessage("Days to manufacture must be greater than 0.");
+    RuleFor(dto => dto.ProductLine)
+    .Must(productLine => new[] { "R", "M", "T", "S" }.Contains(productLine.Trim()))
+    .WithMessage("ProductLine must be 'R', 'M', 'T' or 'S'.");
+    RuleFor(dto => dto.Class)
+    .Must(productClass => new[] { "H", "M", "L" }.Contains(productClass.Trim()))
+    .WithMessage("Class must be 'H', 'M', or 'L'.");
+    RuleFor(dto => dto.Style)
+    .Must(style => new[] { "W", "M", "U" }.Contains(style.Trim()))
+    .WithMessage("Style must be 'W', 'M', or 'U'.");
+    RuleFor(dto => dto.SellStartDate).NotEmpty().WithMessage("Sell start date is required.");
+    RuleFor(dto => dto.SellEndDate)
+        .Null()
+        .When(dto => dto.SellEndDate.HasValue)
+        .WithMessage("Sell end date must be null if set.");
+    RuleFor(dto => dto.DiscontinuedDate)
+        .Null()
+        .When(dto => dto.DiscontinuedDate.HasValue)
+        .WithMessage("Discontinued date must be null if set.")
+        .GreaterThan(dto => dto.SellStartDate)
+        .When(dto => dto.DiscontinuedDate.HasValue)
+        .WithMessage("Discontinued date must be greater than sell start date.");
+  }
+}
---
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Infrastructure/Validators/UpdateProductDtoValidator.cs
@@ -0,0 +1,44 @@
+using FluentValidation;
+using RSMEnterpriseIntegrationsAPI.Application.DTOs;
+
+public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
+{
+  public UpdateProductDtoValidator()
+  {
+    RuleFor(dto => dto.ProductId).NotEmpty().WithMessage("ProductId is required.");
+    RuleFor(dto => dto.Name).NotEmpty().WithMessage("Name is required.");
+    RuleFor(dto => dto.ProductNumber).NotEmpty().WithMessage("ProductNumber is required.");
+    RuleFor(dto => dto.SafetyStockLevel).GreaterThan((short)0).WithMessage("SafetyStockLevel must be greater than 0.");
+    RuleFor(dto => dto.ReorderPoint).GreaterThan((short)0).WithMessage("ReorderPoint must be greater than 0.");
+    RuleFor(dto => dto.StandardCost).GreaterThanOrEqualTo(0).WithMessage("StandardCost must be greater than or equal to 0.");
+    RuleFor(dto => dto.ListPrice).GreaterThanOrEqualTo(0).WithMessage("ListPrice must be greater than or equal to 0.");
+    RuleFor(dto => dto.DaysToManufacture).GreaterThan(0).WithMessage("DaysToManufacture must be greater than 0.");
+    RuleFor(dto => dto.ProductLine)
+     .Must(productLine => new[] { "R", "M", "T", "S" }.Contains(productLine.Trim()))
+     .WithMessage("ProductLine must be 'R', 'M', 'T' or 'S'.");
+    RuleFor(dto => dto.Class)
+    .Must(productClass => new[] { "H", "M", "L" }.Contains(productClass.Trim()))
+    .WithMessage("Class must be 'H', 'M', or 'L'.");
+    RuleFor(dto => dto.Style)
+    .Must(style => new[] { "W", "M", "U" }.Contains(style.Trim()))
+    .WithMessage("Style must be 'W', 'M', or 'U'.");
+    RuleFor(dto => dto.SellStartDate).NotEmpty().WithMessage("SellStartDate is required.");
+    RuleFor(dto => dto.SellEndDate)
+        .Null()
+        .When(dto => dto.SellEndDate.HasValue)
+        .WithMessage("SellEndDate must be null if set.")
+        .GreaterThan(dto => dto.SellStartDate)
+        .When(dto => dto.SellEndDate.HasValue)
+        .WithMessage("SellEndDate must be greater than SellStartDate if set.");
+    RuleFor(dto => dto.DiscontinuedDate)
+        .Null()
+        .When(dto => dto.DiscontinuedDate.HasValue)
+        .WithMessage("DiscontinuedDate must be null if set.")
+        .GreaterThan(dto => dto.SellStartDate)
+        .When(dto => dto.DiscontinuedDate.HasValue)
+        .WithMessage("DiscontinuedDate must be greater than SellStartDate if set.")
+        .LessThanOrEqualTo(DateTime.Now)
+        .When(dto => dto.DiscontinuedDate.HasValue)
+        .WithMessage("DiscontinuedDate must be less than or equal to current date if set.");
+  }
+}
