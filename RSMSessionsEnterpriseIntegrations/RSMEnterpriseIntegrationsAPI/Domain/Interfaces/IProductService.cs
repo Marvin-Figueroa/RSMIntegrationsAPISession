0b1986211b4d8a5d361d12757d99d33f7bdde315
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-07 17:05:33 -0600

    feat: add product related interfaces

---
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Domain/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+namespace RSMEnterpriseIntegrationsAPI.Domain.Interfaces
+{
+  using RSMEnterpriseIntegrationsAPI.Application.DTOs;
+
+  public interface IProductService
+  {
+    Task<GetProductDto?> GetProductById(int id);
+    Task<IEnumerable<GetProductDto>> GetAll();
+    Task<int> CreateProduct(CreateProductDto ProductDto);
+    Task<int> UpdateProduct(UpdateProductDto ProductDto);
+    Task<int> DeleteProduct(int id);
+  }
+}
