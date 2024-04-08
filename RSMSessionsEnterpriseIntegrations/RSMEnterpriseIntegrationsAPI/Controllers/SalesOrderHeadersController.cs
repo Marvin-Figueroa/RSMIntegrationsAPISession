dab33b884556192cb007197647cf4641b924d421
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-08 17:31:55 -0600

    feat: add SalesOrderHeader controller

---
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Controllers/SalesOrderHeadersController.cs
@@ -0,0 +1,49 @@
+namespace RSMEnterpriseIntegrationsAPI.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  using RSMEnterpriseIntegrationsAPI.Application.DTOs;
+  using RSMEnterpriseIntegrationsAPI.Domain.Interfaces;
+
+  [Route("api/sales-order-headers")]
+  [ApiController]
+  public class SalesOrderHeadersController : ControllerBase
+  {
+    private readonly ISalesOrderHeaderService _service;
+
+    public SalesOrderHeadersController(ISalesOrderHeaderService service)
+    {
+      _service = service;
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+      return Ok(await _service.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+      return Ok(await _service.GetSalesOrderHeaderById(id));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      return Ok(await _service.DeleteSalesOrderHeader(id));
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> Create(CreateSalesOrderHeaderDto dto)
+    {
+      return Ok(await _service.CreateSalesOrderHeader(dto));
+    }
+
+    [HttpPut("")]
+    public async Task<IActionResult> Update(UpdateSalesOrderHeaderDto dto)
+    {
+      return Ok(await _service.UpdateSalesOrderHeader(dto));
+    }
+  }
+}
