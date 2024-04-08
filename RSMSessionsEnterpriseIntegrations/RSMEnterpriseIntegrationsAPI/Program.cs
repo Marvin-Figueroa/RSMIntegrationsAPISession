041d8adcf00c6335e8544acc895cac4bd266e72d
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-08 17:33:10 -0600

    feat: add configurations for SalesOrderHeader validations and mapping

--- RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Program.cs
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Program.cs
@@ -15,10 +15,13 @@ builder.Services.AddValidatorsFromAssemblyContaining<CreateProductDtoValidator>(
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateProductDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateProductCategoryDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateProductCategoryDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateSalesOrderHeaderDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateSalesOrderHeaderDtoValidator>();
 
 builder.Services.AddAutoMapper(typeof(DepartmentMappingProfile));
 builder.Services.AddAutoMapper(typeof(ProductMappingProfile));
 builder.Services.AddAutoMapper(typeof(ProductCategoryMappingProfile));
+builder.Services.AddAutoMapper(typeof(SalesOrderHeaderMappingProfile));
 
 builder.Services.AddControllers();
 
@@ -37,6 +40,8 @@ builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
 builder.Services.AddTransient<IProductCategoryService, ProductCategoryService>();
+builder.Services.AddTransient<ISalesOrderHeaderRepository, SalesOrderHeaderRepository>();
+builder.Services.AddTransient<ISalesOrderHeaderService, SalesOrderHeaderService>();
 
 var app = builder.Build();
 
