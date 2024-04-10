8a0e3a1ea7c33ce26eba87742ba0b421d75f28ab
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-09 23:05:59 -0600

    feat: add authentication related dto validators

---
+++ RSMSessionsEnterpriseIntegrations/RSMEnterpriseIntegrationsAPI/Infrastructure/Validators/LoginDtoValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using RSMEnterpriseIntegrationsAPI.Application.DTOs;
+
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+  public LoginDtoValidator()
+  {
+    RuleFor(dto => dto.Email)
+        .NotEmpty().WithMessage("Email is required.")
+        .EmailAddress().WithMessage("Email must be valid.")
+        .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
+
+    RuleFor(dto => dto.Password)
+        .NotEmpty().WithMessage("Password is required.")
+        .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
+        .MaximumLength(100).WithMessage("Password cannot exceed 100 characters.");
+  }
+}
